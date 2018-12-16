99a8da4b98ef17924e22c2274d2373915f1c9892
Matthew Cohen <matt.cohen182@gmail.com>
2018-12-16 16:33:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_pig_latin = PigLatinizer.new
+    @piglatin_solution = user_pig_latin.piglatinize(params[:user_phrase])
+    erb :results
+  end
+
 end
+
